b16390846ba00ba14bb9029b61240d67a808a667
John Boyes <john@agilepathway.co.uk>
2020-03-16 11:49:12 +0700

    Hardcoded deploy url function

--- .circleci/scripts/set-test-site-url.sh
+++ .circleci/scripts/set-test-site-url.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-test_site_url="https://${CIRCLE_BRANCH}--agilepathway-co-uk.netlify.com"
+test_site_url=$(curl https://netlify-function--agilepathway-co-uk.netlify.com/.netlify/functions/deploy-preview-url?commit="${CIRCLE_SHA1}")
 echo "export TEST_SITE_URL=$test_site_url" >> "$BASH_ENV"
 # shellcheck source=/dev/null
 source "$BASH_ENV"
--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	fmt.Println("Finding deploy preview URL for commit:", request.QueryStringParameters["commit"])
+	const deploy_preview_url = "https://netlify-function--agilepathway-co-uk.netlify.com"
+	fmt.Println("Deploy preview url found:", deploy_preview_url)
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello, World",
+		Body:       deploy_preview_url,
 	}, nil
 }
 
