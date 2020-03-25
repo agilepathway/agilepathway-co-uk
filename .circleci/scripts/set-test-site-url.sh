5790208b357ab33c5e60c195441688e476eb477c
John Boyes <john@agilepathway.co.uk>
2020-03-26 00:23:38 +0700

    Drop: Use hardcoded test url temporarily

--- .circleci/scripts/set-test-site-url.sh
+++ .circleci/scripts/set-test-site-url.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 test_site_url=$(curl -H "Authorization: Bearer ${NETLIFY_ACCESS_TOKEN}" -H "User-Agent: CircleCI-${CIRCLE_PROJECT_USERNAME}-${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM}" "$NETLIFY_DEPLOY_PREVIEW_FUNCTION_URL"?commit="${CIRCLE_SHA1}"\&siteid="${NETLIFY_SITE_ID}" )
+test_site_url=https://agilepathway-co-uk.netlify.com
 echo "export TEST_SITE_URL=$test_site_url" >> "$BASH_ENV"
 # shellcheck source=/dev/null
 source "$BASH_ENV"
